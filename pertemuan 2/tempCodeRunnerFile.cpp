b9dd4c627d435826a227e7d047657702fa838da3
AryaSonaGinting <aryasgin27@gmail.com>
2025-03-14 15:28:17 +0700

    pertemuan 2 penggunaan operator

---
+++ pertemuan 2/tempCodeRunnerFile.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    system ("CLS");
+
+    float v, r, pi, a;
+
+    cout << "masukan volume bola :" ;
+    cin >> v;
+
+    cout << "masukan jari jari bola :" ;
+    cin >> r;
+
+    cout << "masukan pi :" ;
+    cin >> pi;
+
+    cout << "masukan luas permukaan bola :" ;
+    cin >> a;
+
+    return 0 ;
+
+}
