2b5d0b9ad5df99a2a5a40c08b9379cfe3d9a64de
AryaSonaGinting <aryasgin27@gmail.com>
2025-05-23 22:39:49 +0700

    Update latihan.cpp

--- pertemuan 3/latihan.cpp
+++ pertemuan 3/latihan.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 
 int main() {
-    int bilangan;
+    int bilangan; // variabel integer
     cout << "Masukkan sebuah bilangan bulat: ";
     cin >> bilangan;
 
-    if (bilangan % 10 == 0) {
+    if (bilangan % 10 == 0) //apakah angka 10 habis dibagi 0
+{
         cout << bilangan << " angka kelipatan dari 10 tuh" << endl;
-    } else if (bilangan % 5 == 0) {
+    } else if (bilangan % 5 == 0) //apakah angka 5 habis dibagi 0
+           {
         cout << bilangan << " angka kelipatan dari 5 guys" << endl;
-    } else {
+    } else // pernyaataan selain dari dua diatas
+           {
         cout << bilangan << " keknya bukan kelipatan dari 5 maupun 10 deh" << endl;
     }
     
