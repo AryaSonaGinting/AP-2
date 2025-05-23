531a8791ceb56cb53eb6d4dbb3b6f74d4cffff91
AryaSonaGinting <aryasgin27@gmail.com>
2025-05-24 00:59:40 +0700

    Update latihanarray.cpp

--- pertemuan 5/latihanarray.cpp
+++ pertemuan 5/latihanarray.cpp
@@ -1,32 +1,35 @@
 #include <iostream>
 using namespace std;
+
 int main(){
-    // penjumlahan matrix 2 x 2
+    // menghitung program penjumlahan matriks 2x2
 
-    int matriks1[2][2];
-    int matriks2[2][2];
-    int hasil[2][2];
+    int matriks1[2][2]; // matriks pertama
+    int matriks2[2][2]; // matriks kedua
+    int hasil[2][2];    // matriks hasil penjumlahan
 
-    cout<<"matriks 1" << endl;
-    for(int i=0; i < 2; i++) {
-        for(int j = 0; j <2; j++) {
-            cout<< "masukkan elemen baris " << i + 1 << " : ";
-            cin >> matriks1[i][j];
+    cout << "matriks 1" << endl;
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            cout << "masukkan elemen baris " << i + 1 << " : ";
+            cin >> matriks1[i][j]; // untuk memasukkan inputan elemen matriks1
         }
     }
-    cout<<"matriks 2" << endl;
-    for(int i=0; i < 2; i++) {
-        for(int j = 0; j <2; j++) {
-            cout<< "masukkan elemen baris " << i + 1 << " : ";
-            cin >> matriks2[i][j];
+
+    cout << "matriks 2" << endl;
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            cout << "masukkan elemen baris " << i + 1 << " : ";
+            cin >> matriks2[i][j]; // untuk memasukkan inputan elemen matriks2
         }
     }
-    cout<<" hasil penjumlahan " << endl;
-    for(int i=0; i < 2; i++) {
-        for(int j = 0; j <2; j++) {
-         hasil[i][j] = matriks1[i][j] + matriks2[i][j];
-         cout << hasil[i][j]<<" ";
-        } cout << endl;
-    } 
 
+    cout << "hasil penjumlahan" << endl;
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            hasil[i][j] = matriks1[i][j] + matriks2[i][j]; // membuat perhitungan penjumlahan elemen
+            cout << hasil[i][j] << " "; // memberikan output hasil
+        }
+        cout << endl; 
+    }
 }
