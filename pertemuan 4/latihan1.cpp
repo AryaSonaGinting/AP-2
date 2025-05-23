8bfb141266a2691c2b3def89865673564c06038d
AryaSonaGinting <aryasgin27@gmail.com>
2025-05-23 22:54:13 +0700

    Update latihan1.cpp

--- pertemuan 4/latihan1.cpp
+++ pertemuan 4/latihan1.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-using namespace std;
+using namespace std; //agar program dapat berjalan langsung tanpa perlu mengkualifikasinya dengan std::
 
 int main() {
-    string kalimat;
-    int i;
+    string kalimat; // variabel teks
+    int i; // variabel integer
 
-    system("CLS");
+    system("CLS"); // clear screen
 
     cout << "Masukkan kalimat: "
-    getline (cin, kalimat);
+    getline (cin, kalimat); // membaca inputan teks string 
 
     // a r y a
     for (i = 0; i < kalimat.length(); i++) {
         kalimat[i] = toupper(kalimat[i]); 
-    }
+    } // mengubah setiap karakter string menjadi huruf kapital
 
     cout << "kalimat kalimat dalam huruf kapital : " << kalimat << endl;
 
