8912946f05552838b6d7ce9f2db742afb9beb833
AryaSonaGinting <aryasgin27@gmail.com>
2025-05-24 00:54:40 +0700

    Update latihan2.cpp

--- pertemuan 4/latihan2.cpp
+++ pertemuan 4/latihan2.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
-#include <string>
+#include <string> // memasukkan file header string
 
 using namespace std;
 
-string Kapital(const string &kalimat) {
+string Kapital(const string &kalimat) // menerima input string, dan membuat string baru dengan kalimat kapital
+ {
     string hasil = kalimat;
     for (char &c : hasil) {
         if (c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o') {
             c = toupper(c);
         }
     }
-    return hasil;
+    return hasil; //kembali ke variabel hasil
 }
 
-int main() {
-    string kalimat;
+int main() { // memulai program
+    string kalimat; // variabel string kalimat
     
     cout << "Masukkan beberapa kalimat: ";
     getline(cin, kalimat);
     
-    string hasil = Kapital(kalimat);
+    string hasil = Kapital(kalimat); // memanggil kalimat dan disimpan sebagai hasil 
     
-    cout << "Hasil: " << hasil << endl;
+    cout << "Hasil: " << hasil << endl; // output hasil
     
     return 0;
 }
