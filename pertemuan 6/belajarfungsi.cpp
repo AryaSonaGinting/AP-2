c2b2493a0a0fb482fe43f928226bbde268679ff3
AryaSonaGinting <aryasgin27@gmail.com>
2025-05-24 01:00:55 +0700

    Update belajarfungsi.cpp

--- pertemuan 6/belajarfungsi.cpp
+++ pertemuan 6/belajarfungsi.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
+// untuk menyapa pengguna
 void sapa(string nama) {
     cout << "halo " << nama << "! selamat datang di AP 2" << endl;
 }
 
 int main() {
-    string namapengguna = "Arya" ;
+    string namapengguna = "Arya"; // nyimpen nama pengguna
 
-    sapa(namapengguna);
+    sapa(namapengguna); // panggil fungsi sapa, kirim nama pengguna
 
-    return 0;
+    return 0; // program selesai
 }
